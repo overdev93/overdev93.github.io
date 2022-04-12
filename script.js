7f17e6b99a75d64b255ca6a36e47564127d8d9c4
overdev <overdevs@gmail.com>
2022-04-12 23:26:33 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+var loadFile = function(event) {
+
+
+
+
+
+  var  output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+
+
+
+
+
+  var description1 = document.getElementById("outputDescription");
+  description1.innerHTML = "Thumbnail on the subscription site, 210x118";
+
+var rect1 = document.getElementById("rect1");
+rect1.style.visibility = "visible";
+/*
+  var description2 = document.getElementById("output2Description");
+  description2.innerHTML = "Thumbnail auf der Start-Seite und der Suche in der minimalen Größe, 210x118";
+
+  var description3 = document.getElementById("output3Description");
+  description3.innerHTML = "Thumbnail auf der Start-Seite und der Suche in der maximalen Größe, 360x202";
+
+    var output = document.getElementById('output');
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.style.height = 300;
+    output.style.width = 300;
+    output.onload = function() {
+      URL.revokeObjectURL(output.src) // free memory
+    }
+    */
+  };
